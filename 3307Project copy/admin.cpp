92398a92b16bf47f42b609aad0b1a007b33f1c46
sterzout <122498255+sterzout@users.noreply.github.com>
2024-11-26 12:25:42 -0500

    Delete 3307Project copy directory

--- 3307Project copy/admin.cpp
+++
@@ -1,8 +0,0 @@
-#include "admin.h"
-#include <iostream>
-
-Admin::Admin(const std::string& name, const std::string& userID, const std::string& email, const std::string& password)
-    : User(name, userID, email, password) {}
-
-
-
